ed5785082bce4508f2afd9cff8df98df3cf4ccc3
marcuspilgram21 <KatzeSchiggy2021>
2021-04-14 11:58:34 +0200

    DMM-002 basic tests (test setup not fully functional), basic layout [PM,MM]

--- app/src/main/java/com/team13/dealmymeal/MainActivity.kt
+++ app/src/main/java/com/team13/dealmymeal/MainActivity.kt
@@ -2,10 +2,26 @@ package com.team13.dealmymeal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val form_textview: TextView = findViewById(R.id.form_showEntry)
+        val form_button: Button = findViewById(R.id.form_save)
+        form_button.setOnClickListener() { v -> Toast.makeText(applicationContext, "Hello", Toast.LENGTH_LONG).show() }
+
+
+
+
     }
+
+
 }
+
+
--- app/src/test/java/com/team13/dealmymeal/ExampleUnitTest.kt
+++ app/src/test/java/com/team13/dealmymeal/ExampleUnitTest.kt
@@ -1,17 +1,52 @@
 package com.team13.dealmymeal
 
+
+import android.widget.CheckBox
+import android.widget.EditText
 import org.junit.Test
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.*
+
+import org.hamcrest.Matchers.not
+
+
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
 class ExampleUnitTest {
+
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun form_buttonTest() {
+        onView(withId(R.id.form_save)).perform(click())
+
+    }
+
+    @Test
+    fun form_checkBoxes() {
+        onView(withId(R.id.check_meat)).perform(click())
+        onView(withId(R.id.check_veggie)).perform(click())
+        onView(withId(R.id.check_special)).perform(click())
+
+
+    }
+
+    @Test
+    fun form_TextInput() {
+        onView(withId(R.id.form_edit)).perform(typeText("test"))
+        onView(withId(R.id.form_edit)).check(matches(withText("test")))
+
     }
 }
