1eed4fd8c1c0ed5f491c06b2abb0387e86ac9c41
marcuspilgram21 <KatzeSchiggy2021>
2021-04-16 15:02:56 +0200

    DMM-002 added multiple tests successfully, fixed espresso dependencies [PM,MM]

--- app/src/androidTest/java/com/team13/dealmymeal/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/team13/dealmymeal/ExampleInstrumentedTest.kt
@@ -1,5 +1,11 @@
 package com.team13.dealmymeal
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +13,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,55 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activityRule: ActivityScenarioRule<MainActivity>
+            = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.team13.dealmymeal", appContext.packageName)
+    fun form_buttonTest() {
+        Espresso.onView(ViewMatchers.withId(R.id.form_save)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.form_cancel)).perform(ViewActions.click())
+
+
+    }
+
+    @Test
+    fun form_checkBoxes() {
+        Espresso.onView(ViewMatchers.withId(R.id.check_meat)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.check_meat)).check(ViewAssertions.matches(ViewMatchers.isChecked()))
+        Espresso.onView(ViewMatchers.withId(R.id.check_veggie)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.check_veggie)).check(ViewAssertions.matches(ViewMatchers.isChecked()))
+        Espresso.onView(ViewMatchers.withId(R.id.check_special)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.check_special)).check(ViewAssertions.matches(ViewMatchers.isChecked()))
+        Espresso.onView(ViewMatchers.withId(R.id.check_meat)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.check_meat)).check(ViewAssertions.matches(ViewMatchers.isNotChecked()))
+        Espresso.onView(ViewMatchers.withId(R.id.check_veggie)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.check_veggie)).check(ViewAssertions.matches(ViewMatchers.isNotChecked()))
+        Espresso.onView(ViewMatchers.withId(R.id.check_special)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.check_special)).check(ViewAssertions.matches(ViewMatchers.isNotChecked()))
+
+    }
+
+    @Test
+    fun form_TextInput() {
+        Espresso.onView(ViewMatchers.withId(R.id.form_edit)).perform(ViewActions.typeText("test"))
+        Espresso.onView(ViewMatchers.withId(R.id.form_edit)).check(ViewAssertions.matches(ViewMatchers.withText("test")))
+
+    }
+
+    @Test
+    fun form_ratingStars() {
+        Espresso.onView(ViewMatchers.withId(R.id.form_ratingBar)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.form_ratingBar)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun form_inputDisplayed() {
+        Espresso.onView(ViewMatchers.withId(R.id.form_edit)).perform(ViewActions.typeText("test"))
+        Espresso.closeSoftKeyboard()
+        Espresso.onView(ViewMatchers.withId(R.id.form_save)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.form_showEntry)).check(ViewAssertions.matches(ViewMatchers.withText("test")))
+
     }
 }
--- app/src/main/java/com/team13/dealmymeal/MainActivity.kt
+++ app/src/main/java/com/team13/dealmymeal/MainActivity.kt
@@ -2,7 +2,9 @@ package com.team13.dealmymeal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
@@ -14,7 +16,17 @@ class MainActivity : AppCompatActivity() {
 
         val form_textview: TextView = findViewById(R.id.form_showEntry)
         val form_button: Button = findViewById(R.id.form_save)
-        form_button.setOnClickListener() { v -> Toast.makeText(applicationContext, "Hello", Toast.LENGTH_LONG).show() }
+        val form_editText: EditText = findViewById(R.id.form_edit)
+        val text = form_editText.text
+        form_button.setOnClickListener() {
+            v ->
+            val toast: Toast = Toast.makeText(applicationContext, "Meal " + text + " added!", Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.TOP, 0, 250)
+            toast.show()
+
+            form_textview.setText(text)
+
+        }
 
 
 
