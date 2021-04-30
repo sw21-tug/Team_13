896fd28192b72e2e06b16415f0769532e3228d97
Maxi <mallweger.m@gmail.com>
2021-04-30 15:50:20 +0200

    DMM-019 added language support for russian, changes on select, added string files for all strings currently in the app [II, MM]

--- app/src/main/java/com/team13/dealmymeal/MainActivity.kt
+++ app/src/main/java/com/team13/dealmymeal/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.team13.dealmymeal
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.LocaleList
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,9 +18,14 @@ import android.view.Menu
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Switch
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.SwitchCompat
+import androidx.core.os.ConfigurationCompat
 import com.google.android.material.navigation.NavigationView
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,7 +34,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loadLocaleValue()
         setContentView(R.layout.main_activity)
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
@@ -35,6 +48,53 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
 
+        val languageButton: Button = findViewById(R.id.changeLanguageButton)
+        languageButton.setOnClickListener() {
+            val languages = arrayOf("русский", "English")
+            val langSelectorBuilder = AlertDialog.Builder(this)
+            langSelectorBuilder.setTitle(R.string.chooseLanguage)
+            langSelectorBuilder.setSingleChoiceItems(languages, -1) { dialog, selection ->
+                when(selection) {
+                    0 -> {
+                        setLocale("ru")
+                    }
+                    1 -> {
+                        setLocale("en")
+                    }
+
+                }
+                recreate()
+                dialog.dismiss()
+            }
+            langSelectorBuilder.create().show()
+        }
+
+
+    }
+
+
+    private fun setLocale(set_locale_string: String) {
+       /* val config: Configuration = resources.configuration
+        config.setLocale(Locale(locale))
+        resources.updateConfiguration(config, resources.displayMetrics)
+
+        */
+
+        val set_locale_list: LocaleList = LocaleList(Locale(set_locale_string))
+        LocaleList.setDefault(set_locale_list)
+        resources.configuration.setLocales(set_locale_list)
+        resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+        val sharedPref = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+        sharedPref.putString("pref_locale_string", set_locale_string)
+        sharedPref.apply()
+
+    }
+
+
+    private fun loadLocaleValue() {
+        val sharedPref = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+        val localeToSet: String = sharedPref.getString("pref_locale_string", "")!!
+        setLocale(localeToSet)
     }
 
 
