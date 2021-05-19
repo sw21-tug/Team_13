bdc1453ef3fea09916d39f7e7cc420e014938d51
nataliekvita <79967052+nataliekvita@users.noreply.github.com>
2021-05-19 10:23:57 +0200

    DMM-020 fixed ratingfiltering and added test [KN,MP]

--- app/src/androidTest/java/com/team13/dealmymeal/MealOverviewFilterRatingTest.kt
+++ app/src/androidTest/java/com/team13/dealmymeal/MealOverviewFilterRatingTest.kt
@@ -1,9 +1,16 @@
 package com.team13.dealmymeal
 
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import junit.framework.TestCase
@@ -18,6 +25,18 @@ import org.junit.runner.RunWith
 class MealOverviewFilterRatingTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    private lateinit var db: DBManager
+    private lateinit var mealDao: MealDao
+
+    @Before
+    public fun setUp() {
+        // get context -- this is an instrumental test
+        // context from the running application
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        // init the db and dao variable
+        db = Room.databaseBuilder(context, DBManager::class.java, "dmm.db").build()
+        mealDao = db.mealDao()
+    }
 
     @Test
     fun filterBtnCheck()
@@ -43,6 +62,7 @@ class MealOverviewFilterRatingTest {
             .perform(ViewActions.typeText("Nudeln"))
             .perform(ViewActions.closeSoftKeyboard())
         Espresso.onView(ViewMatchers.withId(R.id.form_ratingBar)).perform(ViewActions.click())
+        Thread.sleep(500)
         Espresso.onView(ViewMatchers.withId(R.id.form_save)).perform(ViewActions.click())
         Thread.sleep(500)
         Espresso.onView(ViewMatchers.withId(R.id.navigation_overview)).perform(ViewActions.click())
@@ -51,10 +71,9 @@ class MealOverviewFilterRatingTest {
         Espresso.onData(CoreMatchers.anything()).atPosition(2).perform(ViewActions.click());
         Espresso.onView(ViewMatchers.withId(R.id.action_filter_star)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
-
-
-
-
+        onView(withText("Nudeln")).check(matches(isDisplayed()))
+        mealDao.deleteTestItems()
     }
+
 
 }
