6e04dba3d0124108d06c12d63a4469964ebfd562
Gregor Mayr <gremay98@gmail.com>
2021-05-26 10:40:36 +0200

    DMM-014 fixed tests [MG,MP]

--- app/src/androidTest/java/com/team13/dealmymeal/MealOverviewSearchTest.kt
+++ app/src/androidTest/java/com/team13/dealmymeal/MealOverviewSearchTest.kt
@@ -1,8 +1,11 @@
 package com.team13.dealmymeal
 
+import android.content.Context
 import android.graphics.Color
 import android.service.autofill.Validators.not
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
@@ -12,7 +15,10 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import com.team13.dealmymeal.RecyclerViewMatcher.Companion.populate
+import com.team13.dealmymeal.data.DBManager
+import com.team13.dealmymeal.data.Meal
+import com.team13.dealmymeal.data.MealDao
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.FixMethodOrder
@@ -34,47 +40,43 @@ import kotlin.random.Random
 class MealOverviewSearchTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    private lateinit var db: DBManager
+    private lateinit var mealDao: MealDao
+    private val meal = Meal("Spaghetti", listOf(), 0f)
+    private val meal2 = Meal("Burger", listOf(), 0f)
 
     @Before
-    @Test
-    fun switch_overview() {
-        onView(withId(R.id.navigation_overview)).perform(click())
-        onView(withId(R.id.list)).check(matches(isDisplayed()))
-        onView(withId(R.id.list)).perform(populate(15))
-        Thread.sleep(500)
+    public fun setUp() = runBlocking() {
+        // get context -- this is an instrumental test
+        // context from the running application
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        // init the db and dao variable
+        db = Room.databaseBuilder(context, DBManager::class.java, "dmm.db").build()
+        mealDao = db.mealDao()
+        mealDao.deleteAll()
+        mealDao.insert(meal)
+        mealDao.insert(meal2)
     }
 
     @Test
     fun search_visible() {
+        onView(withId(R.id.navigation_overview)).perform(click())
         onView(withId(R.id.action_search)).check(matches(isDisplayed()))
     }
 
     @Test
     fun execute_search() {
-        val r = Random.nextInt(6, 10)
-        onView(withId(R.id.action_search)).perform(click()).perform(typeText("%d".format(r)))
-
-        for (i in 1..15) {
-            if (i != r) {
-                onView(withText("%d Item".format(i))).check(doesNotExist())
-            } else {
-                onView(withText("%d Item".format(i))).check(matches(isDisplayed()))
-            }
-        }
+        onView(withId(R.id.navigation_overview)).perform(click())
+        onView(withId(R.id.action_search)).perform(click()).perform(typeText(meal2.title))
+        onView(withText(meal.title)).check(doesNotExist())
     }
 
     @Test
     fun abort_search() {
+        onView(withId(R.id.navigation_overview)).perform(click())
         onView(withId(R.id.action_search)).perform(click())
         onView(withId(R.id.action_search)).perform(pressBack())
 
-    }
-
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.team13.dealmymeal", appContext.packageName)
     }
 
 }
